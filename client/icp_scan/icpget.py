7ac8d951be7cf109a2c61a909977d299519e3362
SiJiDo <495105034@qq.com>
2021-12-15 14:18:06 +0800

    Update icpget.py
    fix bug

--- client/icp_scan/icpget.py
+++ client/icp_scan/icpget.py
@@ -1,67 +1,62 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from celery import Celery
-import time
-import os
-import json
-from tools.tianyan_beian import  domainget
-from subprocess import Popen
+import requests,sys
+from bs4 import BeautifulSoup
 
-FILEPATH = os.path.split(os.path.realpath(__file__))[0]
-
-if 'DEBUG' in os.environ and os.environ['DEBUG'] == 'False':
-    broker = os.environ['BROKER']
-    backend = os.environ['BACKEND']
-    DEBUG = "False"
-else:
-    DEBUG = "True"
-    broker = 'amqp://guest:guest@127.0.0.1:5672/H_broker'
-    backend = 'amqp://guest:guest@127.0.0.1:5672/H_backend'
-
-app = Celery('H.icpget', broker=broker, backend=backend, )
-app.config_from_object('config')
-
-@app.task
-def run(company, cookie):
-    workdir = FILEPATH + '/tools/'
-    output_file1 = 'output_{}.txt'.format(time.time())
-    output_file2 = 'output_{}.txt'.format(time.time())
-    if DEBUG == 'True':
-        command = './cSubsidiary_mac -n "{}" -c "{}" -p 90 > {}'.format(company,cookie,output_file1)
-    else:
-        command = './cSubsidiary -n "{}" -c "{}" -p 90 > {}'.format(company,cookie,output_file1)
-    cmd = Popen(command,cwd=workdir, shell=True)
-    while 1:
-        ret = Popen.poll(cmd)
-        if ret == 0:
-            break
-
-    f = open(workdir + output_file1, 'r')
-    tmp = []
-    for line in f.readlines():
-        line = line.strip()
-        if("www.tianyancha.com/company/" in line):
-            if("的子公司" in line):
-                tmp.append((line.split("/company/")[1].split(" 的子公司")[0], company))
+def domainget(id, cookie):
+    tianyan_icp_url = "https://www.tianyancha.com/pagination/icp.xhtml?TABLE_DIM_NAME=icp&ps=10&pn=1&id={}".format(id)
+    header={
+        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0",
+        "Cookie":cookie,
+    }
+    icp_result = requests.get(tianyan_icp_url, headers=header)
+    soup = BeautifulSoup(icp_result.text, 'html.parser')
+    result = ""
+    for line in soup.find_all('span'):
+        if('ICP' in str(line) and '号' in str(line)):
+            if("-" in str(line)):
+                result = str(line).split('<span>')[1].split('-')[0]
             else:
-                tmp.append((line.split("/company/")[1].split("\t")[0], line.split("/company/")[1].split("\t")[1]))
+                result = str(line).split('<span>')[1].split('</span>')[0]
 
-    try:
-        os.system("rm -rf {}".format(workdir + output_file1))
-    except:
-        pass
-    
-    content = []
-    all = []
-    for i in tmp:
-        dic = {}
-        dic['name'] = i[1]
-        dic['domain'] = domainget(i[0], cookie)
-        content.append(dic)
-        all = all + dic['domain']
+    icplishi_url = "https://icplishi.com/{}/".format(result)
+    header={
+        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0",
+    }
+    icplishi_result = requests.get(icplishi_url, headers=header)
+    soup = BeautifulSoup(icplishi_result.text, 'html.parser')
+    target = soup.find_all("span")[2:-3]
+    urls = soup.find_all("a")[1:]
 
-    return content, all
+    for i in urls:
+        if (result in str(i)):
+            urls.remove(i)
+        elif ("/company/" in str(i)):
+            urls.remove(i)
+        elif("/site/" in str(i)):
+            urls.remove(i)
+
+    for i in urls:
+        if (result in str(i)):
+            urls.remove(i)
+        elif ("/company/" in str(i)):
+            urls.remove(i)
+        elif("/site/" in str(i)):
+            urls.remove(i)
+
+    l = len(target)
+    lastresult = []
+    for i in range(0,l):
+        #print(str(target[i]).split('<span>')[1].split('</span>')[0] + "\t" + str(urls[i]).split('_blank">')[1].split('</a>')[0])
+        lastresult.append((str(target[i]).split('target="_blank">')[1].split('</a>')[0],str(urls[i]).split('target="_blank">')[1].split('</a>')[0]))
+    # print("=================================================")
+    # for i in lastresult:
+    #     print(i[1])
+
+    return lastresult
+
 
 
 if __name__ == '__main__':
-    pass
+    id = sys.argv[1]
+    cookie = sys.argv[2]
+    print(domainget(id, cookie))
